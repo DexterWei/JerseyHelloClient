43e4ab8eec5473f1beaa385a0ff17cc79533cd5b
dexterwei <dexterwei@163.com>
2015-06-28 08:14:37 -0700

    original bootstrap

--- src/deter/restclient/TestClient.java
+++ src/deter/restclient/TestClient.java
@@ -3,38 +3,38 @@ package deter.restclient;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
+
 import javax.ws.rs.core.MediaType;
+
+import org.codehaus.jettison.json.JSONObject;
  
 public class TestClient{
-  private Integer id;
-  public void SetID(Integer i){
-	  id=i;
-  }
-  public void SendPostRequest(String msg) {
+	private String Type;
+	private String SerialNumber;
+	private String BootStrapUrl;
+	
+	public void Initiate(String type,String sn,String url){
+		Type=type;
+		SerialNumber=sn; 
+		BootStrapUrl=url;//"http://localhost:8080/com.dexter.rest/rest/chats/post"
+	}
+  public void SendPostRequest() {
 	try {
 		Client client = Client.create();
-		WebResource r = client.resource("http://localhost:8080/com.dexter.rest/rest/chats/post");
-		
-		String input = "{\"sender\":\""+id.toString()+"\",\"receiver\":\"0\",\"message\":\""+msg+"\"}";
-		//System.out.println(input);
+		WebResource r = client.resource(BootStrapUrl);
+		JSONObject model = new JSONObject().put("Type",Type).put("SN",SerialNumber);
 		ClientResponse response = r.type("application/json")
-				   .post(ClientResponse.class, input);
+				   .post(ClientResponse.class, model);
 
 		if (response.getStatus() != 201) {
 		   throw new RuntimeException("Failed : HTTP error code : "
 			+ response.getStatus());
 		}
- 
 		String output = response.getEntity(String.class);
- 
 		System.out.println("Output from Server .... \n");
 		System.out.println(output);
- 
 	  } catch (Exception e) {
- 
 		e.printStackTrace();
- 
 	  }
- 
 	}
 }
--- src/deter/restclient/tester/tester.java
+++ src/deter/restclient/tester/tester.java
@@ -10,25 +10,9 @@ import deter.restclient.TestClient;
 
 public class tester {
 	public static void main(String[] args){
-		//prepare 3 clients
-		ArrayList<TestClient> clients = new ArrayList<TestClient>();
-		for(int i=0;i<3;i++){
-			TestClient t = new TestClient();
-			t.SetID(i+1);
-			clients.add(t);
-		}
-
-		//input client number to send current time to the server
-		//echo message history from server
-	    System.out.println("select client #:");
-		Scanner sc = new Scanner(System.in);
-	    int c_id = sc.nextInt();
-		while(c_id>0 && c_id<4){
-			DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
-			Calendar calobj = Calendar.getInstance();
-			clients.get(c_id-1).SendPostRequest(df.format(calobj.getTime()));
-			System.out.println("select client #:");
-			c_id = sc.nextInt();
-		}
+		String BootstrapUrl="http://localhost:8080/com.dexter.rest/rest/chats/post";
+		TestClient client = new TestClient();
+		client.Initiate("fridge", "Simense",BootstrapUrl);
+		client.SendPostRequest();
 	}
 }
