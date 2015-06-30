6a3782683eb43dcb12fa99b83b32f229e4840f34
dexterwei <dexterwei@163.com>
2015-06-30 02:22:34 -0700

    add registration requests

--- src/deter/restclient/TestClient.java
+++ src/deter/restclient/TestClient.java
@@ -18,42 +18,143 @@ public class TestClient{
 	private String SerialNumber;
 	private String BootStrapUrl;
 	private ArrayList<String> RegistrationUrl = new ArrayList<String>();
+	private int RegisterAt;
+	private JSONObject Resources=null;
 	
-	public void Initiate(String man,String type,String sn,String url){
+	
+	public void Initiate(String man,String type,String sn,String url,JSONObject rsc){
 		Manufacturer = man;
 		Model=type;
 		SerialNumber=sn; 
 		BootStrapUrl=url;//"http://localhost:8080/com.dexter.rest/rest/chats/post"
+		Resources = rsc;
 	}
-  public void SendPostRequest() {
-	try {
-		Client client = Client.create();
-		WebResource r = client.resource(BootStrapUrl);
-		JSONObject model = new JSONObject().put("Manufacturer",Manufacturer).put("Model",Model).put("SN",SerialNumber);
-		ClientResponse response = r.type("application/json")
-				   .post(ClientResponse.class, model);
-
-		if (response.getStatus() != 201) {
-		   throw new RuntimeException("Failed : HTTP error code : "
-			+ response.getStatus());
-		}
-		
-		String retstr=response.getEntity(String.class);
-		JSONObject rtn = new JSONObject(retstr);
-		if(!rtn.has("status")){
-			//System.out.println(rtn.get("RegistrationURL").toString());
-			JSONArray urls = (JSONArray)rtn.get("RegistrationURL");
-			for(int i=0;i<urls.length();i++){
-				RegistrationUrl.add((String)urls.get(i));
+	public boolean SendBootstrapRequest() {
+		try {
+			Client client = Client.create();
+			WebResource r = client.resource(BootStrapUrl);
+			JSONObject model = new JSONObject().put("Manufacturer",Manufacturer)
+												.put("Model",Model)
+												.put("SN",SerialNumber);
+			
+			System.out.println("\n Bootstraping .... ");
+			ClientResponse response = r.type("application/json")
+					   .post(ClientResponse.class, model);
+	
+			if (response.getStatus() > 202) {
+			   throw new RuntimeException("Failed : HTTP error code : "
+				+ response.getStatus());
 			}
+			
+			String retstr=response.getEntity(String.class);
+			JSONObject rtn = new JSONObject(retstr);
+			if(!rtn.has("status")){
+				//System.out.println(rtn.get("RegistrationURL").toString());
+				JSONArray urls = (JSONArray)rtn.get("RegistrationUrl");
+				for(int i=0;i<urls.length();i++){
+					RegistrationUrl.add((String)urls.get(i));
+				}
+				System.out.println("Output from Server .... \n");
+				System.out.println(retstr);
+				return true;
+			}else{
+				System.out.println("Output from Server .... \n");
+				System.out.println(retstr);
+				return false;
+			}
+		  } catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		  }
 		}
-		
-		System.out.println("Output from Server .... \n");
-		System.out.println(retstr);
-		for(int i=0;i<RegistrationUrl.size();i++)
-			System.out.println(RegistrationUrl.get(i));
-	  } catch (Exception e) {
-		e.printStackTrace();
-	  }
-	}
+	
+	public void SendRegistrationRequest(int server) {
+		try {
+			Client client = Client.create();
+			WebResource r = client.resource(RegistrationUrl.get(server));
+			JSONObject model = new JSONObject().put("Request","Register")
+												.put("Manufacturer",Manufacturer)
+												.put("Model",Model)
+												.put("SN",SerialNumber)
+												.put("Resources", Resources);
+			
+			System.out.println("\n Signing in .... ");
+			ClientResponse response = r.type("application/json")
+					   .post(ClientResponse.class, model);
+	
+			if (response.getStatus() > 202) {
+			   throw new RuntimeException("Failed : HTTP error code : "
+				+ response.getStatus());
+			}
+			
+			String retstr=response.getEntity(String.class);			
+			System.out.println("Output from Server .... \n");
+			System.out.println(retstr);
+			
+		  } catch (Exception e) {
+			e.printStackTrace();
+		  }
+		}
+	
+	public boolean SendUpdateRequest() {
+		try {
+			Client client = Client.create();
+			WebResource r = client.resource(RegistrationUrl.get(RegisterAt));
+			JSONObject model = new JSONObject().put("Request","Update")
+												.put("Manufacturer",Manufacturer)
+												.put("Model",Model)
+												.put("SN",SerialNumber)
+												.put("Resources", Resources);
+			
+			System.out.println("\n updating .... \n");
+			ClientResponse response = r.type("application/json")
+					   .post(ClientResponse.class, model);
+	
+			if (response.getStatus() > 202) {
+				System.out.println("Failed : HTTP error code : "
+				+ response.getStatus()+response.toString());
+				return false;
+			}
+			
+			String retstr=response.getEntity(String.class);			
+			System.out.println("Output from Server .... \n");
+			System.out.println(retstr);
+			return true;
+		  } catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		  }
+		}
+	
+	public boolean SendDeregisterRequest() {
+		try {
+			Client client = Client.create();
+			WebResource r = client.resource(RegistrationUrl.get(RegisterAt));
+			JSONObject model = new JSONObject().put("Request","De-register")
+												.put("Manufacturer",Manufacturer)
+												.put("Model",Model)
+												.put("SN",SerialNumber)
+												.put("Resources", Resources);
+			
+			System.out.println("\n De-registering .... \n");
+			ClientResponse response = r.type("application/json")
+					   .post(ClientResponse.class, model);
+	
+			if (response.getStatus() > 202) {
+			   System.out.println("Failed : HTTP error code : "
+				+ response.getStatus()+response.toString());
+			   return false;
+			}
+			
+			String retstr=response.getEntity(String.class);			
+			System.out.println("Output from Server .... \n");
+			System.out.println(retstr);
+			return true;
+		  } catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		  }
+		}
+	
 }
+
--- src/deter/restclient/tester/tester.java
+++ src/deter/restclient/tester/tester.java
@@ -6,13 +6,28 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Scanner;
 
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
 import deter.restclient.TestClient;
 
 public class tester {
-	public static void main(String[] args){
-		String BootstrapUrl="http://localhost:8080/com.dexter.rest/rest/chats/post";
+	public static void main(String[] args) throws JSONException{
+		String BootstrapUrl="http://localhost:8080/com.dexter.rest/bootstrap";
 		TestClient client = new TestClient();
-		client.Initiate("XiaoMi","TVBox", "TV20150101001",BootstrapUrl);
-		client.SendPostRequest();
+		client.Initiate("Haier","SmartFridge1", "20150101001",BootstrapUrl,new JSONObject().append("Sensor",2));
+		if(client.SendBootstrapRequest())
+			client.SendRegistrationRequest(0);
+		client.SendUpdateRequest();
+		pause();
+		client.SendDeregisterRequest();
+	}
+	
+	private static void pause(){
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 }
